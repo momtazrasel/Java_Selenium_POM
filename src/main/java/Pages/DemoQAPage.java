3cffcf5b44b8bfc60f7581b0b6dba75111ee3b34
momtazrasel <momtazrasel8@gmail.com>
2024-05-03 23:42:29 +0600

    Project is updated

--- src/main/java/Pages/DemoQAPage.java
+++ src/main/java/Pages/DemoQAPage.java
@@ -2,14 +2,7 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
-<<<<<<< HEAD
-public class DemoQAPage{
-=======
-import java.sql.Driver;
-
 public class DemoQAPage {
->>>>>>> 3b186801fe71812dbfdf0edefd5f7e34f4efe2dd
     public WebDriver driver;
     public ReusableMethod method;
 
@@ -21,16 +14,10 @@ public class DemoQAPage {
 
     public By elementsBlock = By.xpath("(//div[@class='card-body'])[1]");
 
-<<<<<<< HEAD
     public void getElementBlock(){
         ReusableMethod.clickElement(elementsBlock);
     }
 
-=======
-    public void getElementClick(){
-        method.clickElement(elementsBlock);
 
-    }
->>>>>>> 3b186801fe71812dbfdf0edefd5f7e34f4efe2dd
 
 }
--- src/test/java/Tests/DemoQATest.java
+++ src/test/java/Tests/DemoQATest.java
@@ -5,24 +5,13 @@ import Pages.ReusableMethod;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
-<<<<<<< HEAD
 public class DemoQATest extends DemoSetubTest{
 
     @Test
-     public void signInTest(){
+     public void signInTest() {
         DemoQAPage demoQAPage = homePage.demoQAPage();
         demoQAPage.getElementBlock();
-=======
-public class DemoQATest extends Base {
 
 
-    @Test
-     public void signInTest(){
-
->>>>>>> 3b186801fe71812dbfdf0edefd5f7e34f4efe2dd
     }
-
-
-
-
 }
